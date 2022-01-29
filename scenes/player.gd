fe19e9f932f480eeb0fa02833fae61feee3382ef
Zaven Muradyan <zaven@voithos.io>
2022-01-28 23:25:11 -0800

    Add basic player controller.

---
+++ scenes/player.gd
@@ -0,0 +1,141 @@
+extends KinematicBody2D
+
+enum State {
+    # Player can be controlled.
+    CONTROLLABLE,
+    # Player is in a non-gameplay "starting level" or "ending level" animation.
+    ANIMATING,
+}
+
+# The current state of the player.
+var state = State.CONTROLLABLE
+var previous_state = state
+
+# Tracking variables for vertical and horizontal sprite flipping.
+export (bool) var facing_right = true
+var right_side_up = true
+
+# Movement state.
+var velocity = Vector2.ZERO
+var is_moving = false
+var is_airborne = false
+var was_airborne = false
+var is_fast_falling = false
+
+const HORIZONTAL_VEL = 75.0
+const HORIZONTAL_ACCEL = 20 # How quickly we accelerate to max speed
+
+const GRAVITY = 6.0
+const GRAVITY_DECREASE_THRESHOLD = 10 # The speed below which gravity is decreased.
+const GRAVITY_DECREASE_MULTIPLIER = 0.5 # The amount of decrease for low gravity (at the height of a jump).
+const JUMP_VEL = 160
+const TERM_VEL = JUMP_VEL * 2
+const FAST_FALL_MULTIPLIER = 1.7 # How much faster fast fall is compared to gravity
+const JUMP_RELEASE_MULTIPLIER = 0.5 # Multiplied by velocity if button released during the initial part of the jump.
+
+# Squash and stretch
+var squash_stretch_scale = Vector2.ONE
+const JUMP_SQUASH_STRETCH = Vector2(0.8, 1.2)
+const LAND_SQUASH_STRETCH = Vector2(1.5, 0.4)
+const SQUASH_LERP_SPEED = 10
+
+func _ready():
+    add_to_group("player")
+    _update_sprite_flip()
+
+func _physics_process(delta):
+    if state != State.CONTROLLABLE:
+        return
+
+    _animate_squash_stretch(delta)
+    _move_player(delta)
+    _update_sprite_flip()
+
+func _move_player(delta):
+    was_airborne = is_airborne
+    
+    var target_horizontal = 0
+    var fall_multiplier = 1.0
+    is_moving = false
+
+    if Input.is_action_pressed("move_left"):
+        target_horizontal -= HORIZONTAL_VEL
+        is_moving = true
+    if Input.is_action_pressed("move_right"):
+        target_horizontal += HORIZONTAL_VEL
+        is_moving = true
+
+    if Input.is_action_just_pressed("jump") and _is_on_surface():
+        _jump()
+    
+    if target_horizontal != 0:
+        facing_right = target_horizontal > 0
+
+    # Apply gravity and fast falling
+    if is_airborne and Input.is_action_just_released("jump"):
+        is_fast_falling = true
+        if velocity.y < 0:
+            velocity.y *= JUMP_RELEASE_MULTIPLIER
+
+    if is_fast_falling:
+        fall_multiplier = FAST_FALL_MULTIPLIER
+
+    # When we're nearing the top of the jump, decrease gravity.
+    var grav_multiplier = 1.0 if is_fast_falling or abs(velocity.y) > GRAVITY_DECREASE_THRESHOLD else GRAVITY_DECREASE_MULTIPLIER
+    velocity.y = min(TERM_VEL, velocity.y + GRAVITY * grav_multiplier * fall_multiplier)
+
+    # Lerp horizontal movement
+    velocity.x = lerp(velocity.x, target_horizontal, HORIZONTAL_ACCEL * delta)
+
+
+    velocity = move_and_slide(velocity, Vector2.UP)
+    
+    if was_airborne and _is_on_surface():
+        _landed()
+    elif !was_airborne and !_is_on_surface():
+        # Fell off a cliff.
+        is_airborne = true
+
+    if !is_airborne:
+        if is_moving:
+            #$animation.play("run")
+            pass
+        else:
+            #$animation.play("idle")
+            pass
+
+func _jump():
+    is_airborne = true
+    is_fast_falling = false
+    velocity.y = -JUMP_VEL
+    #$animation.play("jump")
+    _apply_jump_squash_stretch()
+
+func _landed():
+    is_airborne = false
+
+func _is_on_surface():
+    # TODO: Make this work with the "mirror" world.
+    return is_on_floor()
+
+func _animate_squash_stretch(delta):
+    if is_airborne and velocity.y < 0:
+        _apply_jump_squash_stretch()
+    else:
+        # We could make this framerate-independent by using delta properly here, but we don't
+        # have to since the delta comes from physics_process.
+        # https://www.construct.net/en/blogs/ashleys-blog-2/using-lerp-delta-time-924
+        var lerp_val = SQUASH_LERP_SPEED * delta
+        assert(lerp_val <= 1.0)
+        squash_stretch_scale.x = lerp(squash_stretch_scale.x, 1.0, lerp_val)
+        squash_stretch_scale.y = lerp(squash_stretch_scale.y, 1.0, lerp_val)
+    $sprite.scale = squash_stretch_scale
+
+func _apply_jump_squash_stretch():
+    squash_stretch_scale.x = range_lerp(abs(velocity.y), 0, JUMP_VEL, 1.0, JUMP_SQUASH_STRETCH.x)
+    squash_stretch_scale.y = range_lerp(abs(velocity.y), 0, JUMP_VEL, 1.0, JUMP_SQUASH_STRETCH.y)
+    $sprite.scale = squash_stretch_scale
+
+func _update_sprite_flip():
+    $sprite.flip_h = !facing_right
+    $sprite.flip_v = !right_side_up
