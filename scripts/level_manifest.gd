7391925fbd4559c2375c7f43bb26ecf35bb98835
DiegoRGonzalez <stargazingcow@gmail.com>
2022-01-30 02:35:47 -0800

    Add vertical level

--- scripts/level_manifest.gd
+++ scripts/level_manifest.gd
@@ -10,6 +10,7 @@ enum {
     DEMO_ZAVEN = 4,
     TUTORIAL = 5,
     FLING = 6,
+    VERTICAL = 7,
 }
 
 var packed_scenes := {
@@ -19,6 +20,7 @@ var packed_scenes := {
     DEMO_ZAVEN: load("res://scenes/demos/demo_zaven.tscn"),
     TUTORIAL: load("res://scenes/levels/tutorial_level.tscn"),
     FLING: load("res://scenes/levels/fling_level.tscn"),
+    VERTICAL: load("res//scenes/levels/vertical_level.tscn")
 }
 
 const START_LEVEL := DEMO_BASE
