b756a3571a91c837d3819c6f4262adafa41a4a97
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-01-29 22:17:03 -0800

    Add a restart button to the pause screen.

--- scenes/screens/pause_screen.gd
+++ scenes/screens/pause_screen.gd
@@ -7,6 +7,12 @@ func _init().(ScreenController.PAUSE, PAUSE_MODE_PROCESS) -> void:
     pass
 
 
-func _on_Button_pressed() -> void:
+func _on_RestartButton_pressed() -> void:
+    sfx.play(sfx.BUTTON_PRESS)
+    screen.set_pause(false)
+    screen.game_screen.level_logic.reset()
+
+
+func _on_UnpauseButton_pressed() -> void:
     sfx.play(sfx.BUTTON_PRESS)
     screen.set_pause(false)
