45d03ecadc301b5f0dae0c0362690dd7196e4584
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-01-29 22:22:55 -0800

    Make the crystal a simple Node2D.

--- scenes/crystal.gd
+++ scenes/crystal.gd
@@ -1,8 +1,8 @@
 class_name Crystal
-extends RigidBody2D
+extends Node2D
 
 
-func _on_Crystal_body_entered(body: Node) -> void:
+func _on_Area2D_body_entered(body: Node) -> void:
     if body.is_in_group("player"):
         print("Player touched crystal")
         body.on_touched_crystal(self)
