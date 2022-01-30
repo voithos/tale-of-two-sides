03b4a7b55faaea5bb294ca35ff4af694727fa471
DiegoRGonzalez <stargazingcow@gmail.com>
2022-01-30 11:50:39 -0800

    keep track of dialogs

--- scenes/dialog_trigger.gd
+++ scenes/dialog_trigger.gd
@@ -4,14 +4,12 @@ extends Node2D
 export(String) var dialog = "intro"
 export(Vector2) var box_size = Vector2(10,10)
 
-var started = false;
 onready var player = $"../player"
 
 func _ready():
     $Area2D/CollisionShape2D.get_shape().set_extents(box_size);
 
 func create_dialog():
-    started = true;
     var new_dialog = Dialogic.start(dialog)
     new_dialog.connect("timeline_end", self, "_dialog_end")
     player.enter_cutscene();
@@ -21,5 +19,7 @@ func _dialog_end(signal_type):
     player.exit_cutscene();
 
 func _on_Area2D_body_entered(body):
-    if !started && body.is_in_group("player") && body.state == body.State.CONTROLLABLE:
+    var spoken = npc_store.has_spoken_with_npc(dialog)
+    if !spoken && body.is_in_group("player") && body.state == body.State.CONTROLLABLE:
         create_dialog();
+        npc_store.record_npc_interaction(dialog)
