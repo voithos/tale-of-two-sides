d8eea33fb19ed0659697896e46de284103f566a3
DiegoRGonzalez <stargazingcow@gmail.com>
2022-01-30 13:31:45 -0800

    make some dialog trigger zones bigger

--- scenes/dialog_trigger.gd
+++ scenes/dialog_trigger.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 
 export(String) var dialog = "intro"
-export(Vector2) var box_size = Vector2(10,10)
 
 onready var player = $"../player"
 
 func _ready():
-    $Area2D/CollisionShape2D.get_shape().set_extents(box_size);
+    pass;
 
 func _on_Area2D_body_entered(body):
     if body.is_in_group("player"):
